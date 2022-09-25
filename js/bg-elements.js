b5fcd98a9a1d436104ffa28c93fc46c830866b0f
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2022-09-25 16:31:57 +0300

    feat: add renderTopAnime and add renderAnimeList

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,10 +1,10 @@
 const bgElements = () => {
   const elements = document.querySelectorAll(".set-bg");
+  //console.log(elements);
 
-  for (let i = 0; i < elements.length; i++) {
-    const src = elements[i].dataset.setbg;
-    elements[i].style.backgroundImage = `url(${src})`;
-  }
+  elements.forEach((elem) => {
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`; //буз использ переменной src
+  });
 };
 
 bgElements();
