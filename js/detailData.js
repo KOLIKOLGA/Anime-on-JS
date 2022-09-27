8fa936365501ecba2ff8970e4d3e59b985f76575
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2022-09-27 07:20:09 +0300

    feat: add detailData.js

--- js/detailData.js
+++ js/detailData.js
@@ -1,4 +1,4 @@
-const mainData = () => {
+const detailData = () => {
   const renderGanreList = (ganres) => {
     const dropdownBlock = document.querySelector(".header__menu .dropdown");
 
@@ -6,19 +6,19 @@ const mainData = () => {
       dropdownBlock.insertAdjacentHTML(
         "afterbegin",
         `
-      <li><a href="./categories.html?ganre=${ganre}">${ganre}</a></li>
-      `
+    <li><a href="./categories.html?ganre=${ganre}">${ganre}</a></li>
+    `
       );
     });
   };
 
   const renderAnimeList = (array, ganres) => {
-    const wrapper = document.querySelector(".product .col-lg-8");
+    const wrapper = document.querySelector(".product-page .col-lg-8");
 
     ganres.forEach((ganre) => {
       const productBlock = document.createElement("div");
       const listBlock = document.createElement("div");
-      const list = array.filter((item) => item.ganre === ganre);
+      const list = array.filter((item) => item.tags.includes(ganre));
 
       listBlock.classList.add("row");
       productBlock.classList.add("mb-5");
@@ -26,19 +26,19 @@ const mainData = () => {
       productBlock.insertAdjacentHTML(
         "afterbegin",
         `
-      <div class="row">
-      <div class="col-lg-8 col-md-8 col-sm-8">
-          <div class="section-title">
-              <h4>${ganre}</h4>
-          </div>
-      </div>
-      <div class="col-lg-4 col-md-4 col-sm-4">
-          <div class="btn__all">
-              <a href="/categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
-          </div>
-      </div>
-      </div>
-        `
+    <div class="row">
+    <div class="col-lg-8 col-md-8 col-sm-8">
+        <div class="section-title">
+            <h4>${ganre}</h4>
+        </div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4">
+        <div class="btn__all">
+            <a href="/categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
+        </div>
+    </div>
+    </div>
+      `
       );
 
       list.forEach((item) => {
@@ -48,27 +48,27 @@ const mainData = () => {
           tagsBlock.insertAdjacentHTML(
             "afterbegin",
             `
-             <li>${tag}</li>
-            `
+           <li>${tag}</li>
+          `
           );
         });
 
         listBlock.insertAdjacentHTML(
           "afterbegin",
           `
-        <div class="col-lg-4 col-md-6 col-sm-6">
-        <div class="product__item">
-            <div class="product__item__pic set-bg" data-setbg="${item.image}">
-                <div class="ep">${item.rating} / 10</div>
-                <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
-            </div>
-            <div class="product__item__text">
-                ${tagsBlock.outerHTML}
-                <h5><a href="/anime-details.html?itemId=${item.id}">${item.title}</a></h5>
-            </div>
-        </div>
-        </div>
-         `
+      <div class="col-lg-4 col-md-6 col-sm-6">
+      <div class="product__item">
+          <div class="product__item__pic set-bg" data-setbg="${item.image}">
+              <div class="ep">${item.rating} / 10</div>
+              <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+          </div>
+          <div class="product__item__text">
+              ${tagsBlock.outerHTML}
+              <h5><a href="/anime-details.html?itemId=${item.id}">${item.title}</a></h5>
+          </div>
+      </div>
+      </div>
+       `
         );
       });
 
@@ -87,12 +87,12 @@ const mainData = () => {
       wrapper.insertAdjacentHTML(
         "beforeend",
         `
-        <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
-          <div class="ep">${item.rating}/ 10</div>
-          <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
-          <h5><a href="/anime-details.html">${item.title}</a></h5>
-        </div>
-        `
+      <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+        <div class="ep">${item.rating}/ 10</div>
+        <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+        <h5><a href="/anime-details.html">${item.title}</a></h5>
+      </div>
+      `
       );
     });
     wrapper.querySelectorAll(".set-bg").forEach((elem) => {
@@ -106,15 +106,23 @@ const mainData = () => {
     .then((response) => response.json())
     .then((data) => {
       const ganres = new Set();
+      const ganreParams = new URLSearchParams(window.location.search).get(
+        "ganre"
+      );
 
       data.forEach((item) => {
         ganres.add(item.ganre);
       });
 
       renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
-      renderAnimeList(data, ganres);
+      if (ganreParams) {
+        renderAnimeList(data, [ganreParams]);
+      } else {
+        renderAnimeList(data, ganres);
+      }
+
       renderGanreList(ganres);
     });
 };
 
-mainData();
+detailData();
